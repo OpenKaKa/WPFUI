b4a100d89ba185e034cd1df184564ba5c68b8710
Terrence <1908723707@qq.com>
2023-06-22 23:02:13 +0800

    添加了导航菜单和子菜单UI界面.

---
+++ Navigation Menu With Sub Menu/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Navigation_Menu_With_Sub_Menu
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Navigation Menu With Sub Menu/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace Navigation_Menu_With_Sub_Menu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MaxBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    WindowState = WindowState.Normal;
+                }
+            }
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
